61c5213a81675649bf01299798eff7039593898d
Eugene D. <99919423+eugenedey@users.noreply.github.com>
2025-08-19 10:27:14 -0500

    Added chat with cat page
    Added chat with cat page

---
+++ SailorTheCat/Pages/Chat.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SailorTheCat.Pages
+{
+    public class ChatModel : PageModel
+    {
+        private readonly ILogger<ChatModel> _logger;
+
+        public ChatModel(ILogger<ChatModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+            string apikey = config["apikey"];
+            ViewData["apikey"] = apikey;
+        }
+    }
+}
